184e1267876cbdabe3346cd3ffddc200d59fd1ba
Lane <mail_zlj@163.com>
2015-05-29 22:41:46 +0800

    A simple web that can work with a homepage.

---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,10 @@
+class BaseController < ApplicationController
+  # skip_before_filter :check_logged
+  # before_filter :check_session
+  #
+  # private
+  #
+  # def check_session
+  #   redirect_to login_path unless user?
+  # end
+end
